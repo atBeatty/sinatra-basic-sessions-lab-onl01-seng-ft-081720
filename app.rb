1ebca7ac3ca201358f4f779038463633cc106839
atBeatty <adam.t.beatt@gmail.com>
2020-10-01 10:45:58 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do 
+        enable :session
+        set :session_secret, "secretsarefun"
+    end
+
+    get '/' do 
+        erb :index
+    end
+
+    post '/checkout' do 
+        session[:item] = params[:item]
+        @session = session
+        binding.pry
+        erb :checkout
+    end 
+
+
+
+
+
 end
